9f6011929590f7e4d55dec42c163d853974ec3de
silverbaymaps <80330720+silverbaymaps@users.noreply.github.com>
2021-04-06 10:47:31 -0400

    Add files via upload

--- campusPoints.js
+++ campusPoints.js
@@ -53,7 +53,7 @@ var quadrant3  = {
         { id: "Store", lat: 43.69623575730455, lon: -73.50572717236851, top: 723, left: 765 },
         { id: "BoydNW", lat: 43.69611940094004, lon: -73.5062582497474, top: 807, left: 776 },
         { id: "BoydSE", lat: 43.695712151920006, lon: -73.50579959201501, top: 733, left: 870 },
-        { id: "BoydSW", lat: 43.695712151920006, lon: -73.50619387673234, top: 733, left: 870 },
+        { id: "BoydSW", lat: 43.695712151920006, lon: -73.50619387673234, top: 795, left: 866 },
         { id: "Parlin", lat: 43.696846624462985, lon: -73.50686442897963, top: 900, left: 632 },
         { id: "Hillcrest", lat: 43.6963385385604, lon: -73.5071460609338, top: 950, left: 741 },
         { id: "Elm", lat: 43.695960380224456, lon: -73.50728821801485, top: 968, left: 824 },
@@ -61,10 +61,75 @@ var quadrant3  = {
     ]
 };
 
+
+var quadrant4  = {
+    id: "quadrant4",
+    nBorder: 43.6956093695677,
+    sBorder: 43.693990485955375,
+    eBorder: -73.50115668856414,
+    wBorder: -73.50513708631262,
+    pxPerLat: 212514,
+    pxPerLon: 152016,
+    points: [
+        { id: "Erc", lat: 43.695250508352764, lon: -73.50301905227533, top: 324, left: 963 },
+        { id: "Collins", lat: 43.694785975619304, lon: -73.50477117081992, top: 573, left: 1068 },
+        { id: "EastDock", lat: 43.69502799024445, lon: -73.50368761115456, top: 420, left: 1018 },
+        { id: "walkN", lat: 43.695263024569236, lon: -73.50526648398909, top: 651, left: 963 },
+        { id: "walkJ", lat: 43.695022615572455, lon: -73.5045190462283, top: 525, left: 1020 },
+        { id: "BoatHouseE", lat: 43.694864561597875, lon: -73.5045109995932, top: 525, left: 1065 },
+        { id: "SilverSpray", lat: 43.69439621518591, lon: -73.50425618975748, top: 501, left: 1148 },
+        { id: "TennisE", lat: 43.69428858225673, lon: -73.50503939478266, top: 612, left: 1168 }
+    ]
+};
+
+var quadrant5  = {
+    id: "quadrant5",
+    nBorder: 43.6956093695677,
+    sBorder: 43.693990485955375,
+    eBorder: -73.50513708631262,
+    wBorder: -73.50961101065408,
+    pxPerLat: 212514,
+    pxPerLon: 152016,
+    points: [
+        { id: "InnNE", lat: 43.69555991762921, lon: -73.50564938831984, top: 708, left: 901 },
+        { id: "InnSE", lat: 43.69511000006835, lon: -73.50569766807818, top: 711, left: 1003 },
+        { id: "AudSE", lat: 43.69444675336285, lon: -73.50614291477305, top: 781, left: 1142 },
+        { id: "AudNW", lat: 43.69481134602297, lon: -73.50642722890554, top: 827, left: 1054 },
+        { id: "Munn", lat: 43.694301303544805, lon: -73.50662839456533, top: 855, left: 1162 },
+        { id: "Hepbron", lat: 43.69406664375081, lon: -73.50714874310854, top: 935, left: 1218 },
+        { id: "Hemlock", lat: 43.695218601167355, lon: -73.50726944250438, top: 960, left: 973 },
+        { id: "Overlook", lat: 43.69449717588098, lon: -73.50752425238622, top: 1001, left: 1102 },
+        { id: "BoydSW", lat: 43.695712151920006, lon: -73.50619387673234, top: 795, left: 866 },
+        { id: "Admin", lat: 43.69572184835643, lon: -73.50663912343141, top: 858, left: 870 }
+    ]
+};
+
+var quadrant6  = {
+    id: "quadrant6",
+    nBorder: 43.693990485955375,
+    sBorder: 43.692543272140895,
+    eBorder: -73.50115668856414,
+    wBorder: -73.50513708631262,
+    pxPerLat: 212514,
+    pxPerLon: 152016,
+    points: [
+        { id: "BayBeach", lat: 43.6933476288075, lon: -73.5044397120463, top: 530, left: 1357 },
+        { id: "FirePit", lat: 43.69306545066319, lon: -73.50418892551016, top: 483, left: 1431 },
+        { id: "BB-NE", lat: 43.693633684332696, lon: -73.50489300537224, top: 593, left: 1305 },
+        { id: "BB-SE", lat: 43.69338641607963, lon: -73.50491848635703, top: 596, left: 1354 },
+        { id: "Backboard", lat: 43.69306642035332, lon: -73.50533422874054, top: 643, left: 1422 },
+        { id: "LizGray", lat: 43.69242157528539, lon: -73.50407224941253, top: 466, left: 1565 },
+    ]
+};
+
+
 var quadrantArray = [
     quadrant1,
     quadrant2,
-    quadrant3
+    quadrant3,
+    quadrant4,
+    quadrant5,
+    quadrant6
 ]
 
 
@@ -90,7 +155,28 @@ var campusPlaces = [
     ["Elm", 970, 840],
     ["Juniper", 993, 890],
     ["Hillcrest", 947, 747],
-    ["Craft", 900, 826]
-    // ["", , ],
-    // ["", , ],
+    ["Craft", 900, 826],
+    ["Collins", 577, 1066],
+    ["Shuffleboard", 574, 1042],
+    ["DiscGolf", 588, 1081],
+    ["Boathouse", 531, 1076],
+    ["Tennis", 627, 1200],
+    ["Baseball", 581, 875],
+    ["Inn", 695, 951],
+    ["Auditorium", 755, 1102],
+    ["Field", 872, 1028],
+    ["Munn", 853, 1153],
+    ["Hemlock", 945, 978],
+    ["Overlook", 995, 1071],
+    ["Hepbron", 926, 1172],
+    ["Sycamore", 978, 924],
+    ["Locust", 905, 1102],
+    ["BayBeach", 533, 1358],
+    ["FirePit", 515, 1433],
+    ["Basketball", 594, 1328],
+    ["Volleyball", 632, 1360],
+    ["TennisBackboard", 616, 1412]
+    //["", ],
+    // ["",  ],
+    // ["",  ],
 ];
